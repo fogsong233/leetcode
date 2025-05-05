6c5135eb035133d348bafe0791d1307e26aa7936
fogsong233 <75871375+fogsong233@users.noreply.github.com>
2025-05-05 19:10:16 +0800

    115 and 60

---
+++ Users/kacent/code/leetcode/115.不同的子序列.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=115 lang=cpp
+ *
+ * [115] 不同的子序列
+ */
+#include <cstdio>
+#include <string>
+#include <string_view>
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution {
+  vector<vector<int>> cache;
+
+public:
+  int numDistinct(string s, string t) {
+    cache = vector<vector<int>>(s.size() + 1, vector<int>(t.size() + 1, -1));
+    return numDistinct(string_view(s), string_view(t));
+  }
+  int numDistinct(string_view s, string_view t) {
+    if (cache[s.size()][t.size()] != -1) {
+      return cache[s.size()][t.size()];
+    }
+    int res = 0;
+    if (t.size() == 0) {
+      return 1;
+    }
+    if (s.size() == 0) {
+      return 0;
+    }
+    auto subS = s.substr(1);
+    auto subT = t.substr(1);
+    res += numDistinct(subS, t); // 不选
+    if (t[0] == s[0]) {
+      res += numDistinct(subS, subT);
+    }
+    cache[s.size()][t.size()] = res;
+    return res;
+  }
+};
+// @lc code=end
---
+++ Users/kacent/code/leetcode/60.排列序列.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=60 lang=cpp
+ *
+ * [60] 排列序列
+ */
+
+#include <numeric>
+#include <ranges>
+#include <string>
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution {
+  vector<bool> used;
+  vector<int> rang;
+
+public:
+  string getPermutation(int n, int k) {
+    used = vector<bool>(n, false);
+    rang = vector<int>(n);
+    iota(rang.begin(), rang.end(), 0);
+    return findKthPermOfN(k, n, "");
+  }
+
+  int fact(int n) { return n == 1 ? 1 : n * fact(n - 1); }
+
+  string findKthPermOfN(int k, int n, string prefix) {
+    int kth;
+    if (n == 1) {
+      kth = 1;
+    } else {
+      kth = fact(n - 1);
+    }
+    prefix += ('0' +
+               *(ranges::find_if(rang,
+                                 [&](int v) {
+                                   if (used[v]) {
+                                     return false;
+                                   }
+                                   if (k <= kth) {
+                                     used[v] = true;
+                                     return true;
+                                   }
+                                   k -= kth;
+                                   return false;
+                                 })) +
+               1);
+    if (prefix.size() == used.size()) {
+      return prefix;
+    }
+    return findKthPermOfN(k, n - 1, prefix);
+  }
+};
+// @lc code=end
